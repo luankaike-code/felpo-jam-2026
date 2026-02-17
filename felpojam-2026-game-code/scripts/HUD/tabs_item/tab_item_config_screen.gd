c0e4cf681b2f25ddb39b2ac399dad94114bc5831
luankaike-code <luankaikemacksantos@gmail.com>
2026-02-17 18:46:37 -0300

    Update screen configurations

--- felpojam-2026-game-code/scripts/HUD/tabs_item/tab_item_config_screen.gd
+++ felpojam-2026-game-code/scripts/HUD/tabs_item/tab_item_config_screen.gd
@@ -1,6 +1,7 @@
 extends TabBar
 
 @onready var resolution_drop_menu := $MarginContainer/ScrollContainer/MarginContainer/VBoxContainer/ScreenResolution/Resolutions as OptionButton
+@onready var screen_mode_drop_menu := $MarginContainer/ScrollContainer/MarginContainer/VBoxContainer/SreenMode/ScreenModes as OptionButton
 
 var resolutions: Dictionary[String, Vector2i] = {
 	"3840X2160(16:9)": Vector2i(3840, 2160),
@@ -10,17 +11,50 @@ var resolutions: Dictionary[String, Vector2i] = {
 	"648X648(1:1)": Vector2i(648, 648),
 }
 
+var screen_modes: Dictionary[String, Window.Mode] = {
+	"tela cheia": Window.MODE_FULLSCREEN,
+	"janela": Window.MODE_WINDOWED,
+}
+
+var current_resolution: int
+
 func _ready() -> void:
 	resolution_drop_menu.item_selected.connect(resolution_selected)
-	populate_resolution_drop_menu()
+	screen_mode_drop_menu.item_selected.connect(screen_mode_selected)
 	
-func populate_resolution_drop_menu() -> void:
+	populate_drop_menu_with_dict(resolution_drop_menu, resolutions)
+	populate_drop_menu_with_dict(screen_mode_drop_menu, screen_modes)
+
+
+func populate_drop_menu_with_dict(drop_menu: OptionButton, dict: Dictionary) -> void:
+	for key in dict.keys():
+		drop_menu.add_item(key)
+
+func populate_drop_menus() -> void:
+	for resolution in resolutions.keys():
+		resolution_drop_menu.add_item(resolution)
 	for resolution in resolutions.keys():
 		resolution_drop_menu.add_item(resolution)
 
 func resolution_selected(resolution_index: int) -> void:
+	current_resolution = resolution_index
 	var resolution_key: String = resolutions.keys()[resolution_index]
 	var resolution := resolutions[resolution_key]
 
-	get_window().size = resolution
-	get_window().content_scale_size = resolution
+	var window := get_window()
+	window.size = resolution
+	window.content_scale_size = resolution
+
+func screen_mode_selected(screen_mode_index: int) -> void:
+	var screen_mode_key: String = screen_modes.keys()[screen_mode_index]
+	var screen_mode := screen_modes[screen_mode_key]
+	
+	var window := get_window()
+	window.content_scale_mode = Window.CONTENT_SCALE_MODE_VIEWPORT
+	window.mode = screen_mode
+	
+	resolution_selected(current_resolution)
+	
+	
+	
+	
