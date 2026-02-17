73a0c5585a2b277c6cc98d2b14c0d6eb1003e46e
luankaike-code <luankaikemacksantos@gmail.com>
2026-02-17 10:32:39 -0300

    Create PaperTeleporter

--- felpojam-2026-game-code/scripts/entitys/paper_teleporter.gd
+++ felpojam-2026-game-code/scripts/entitys/paper_teleporter.gd
@@ -1,4 +1,22 @@
-extends Stand
+class_name PaperTeleporter extends Stand
+
+@export var linked_teleporter: PaperTeleporter
+
+var teleported_paper: Paper
+
+func _ready() -> void:
+	area_exited.connect(deslinked_teleported_paper)
+
+func deslinked_teleported_paper(area: Area2D):
+	if area == teleported_paper:
+		teleported_paper = null
+
+func teleporter_paper(paper: Paper):
+	teleported_paper = paper
+	paper.global_position = global_position
 
 func place_item(item: Node2D) -> bool:
+	if item is Paper and linked_teleporter and item != teleported_paper:
+		linked_teleporter.teleporter_paper(item)
+		return true
 	return false
