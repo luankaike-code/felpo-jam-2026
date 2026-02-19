c394238b233839693c70ed2dc2271ac99e989a3f
luankaike-code <luankaikemacksantos@gmail.com>
2026-02-18 21:28:14 -0300

    Remove spots feature

--- felpojam-2026-game-code/scripts/data/runes_data.gd
+++ felpojam-2026-game-code/scripts/data/runes_data.gd
@@ -18,15 +18,6 @@ static var names_str: Dictionary[names, String] = {
 	names.order: "Ordem"
 }
 
-static var colors: Dictionary[names, Color] = {
-	names.fire: Color(0.839, 0.364, 0.322),
-	names.water: Color(0.337, 0.319, 0.84),
-	names.air: Color(0.84, 0.84, 0.84),
-	names.earth: Color(0.398, 0.134, 0.114),
-	names.chaos: Color(0.38, 0.107, 0.087),
-	names.order: Color(0.87, 0.639, 0.139),
-}
-
 static var packed: Dictionary[names, PackedScene] = {
 	names.fire: load("res://scenes/entitys/runes/fire_rune.tscn"),
 	names.water: load("res://scenes/entitys/runes/fire_rune.tscn")
