acd4f17a5f1b579fc2fe024b993dca27f2e48c70
luankaike-code <luankaikemacksantos@gmail.com>
2026-02-19 22:19:17 -0300

    Feature paper so can two rines

--- felpojam-2026-game-code/scripts/entitys/paper/paper.gd
+++ felpojam-2026-game-code/scripts/entitys/paper/paper.gd
@@ -1,5 +1,11 @@
 class_name Paper extends Draggable
 
+var runes: Array[Rune]
+
 func add_rune(rune_scene: Rune, global_position: Vector2) -> void:
+	if runes.size() >= 2:
+		return
+		
 	rune_scene.position = to_local(global_position)
 	add_child(rune_scene)
+	runes.push_front(rune_scene)
