b10baf8beb6148810573f12ee7794ef562da6a7a
luankaike-code <luankaikemacksantos@gmail.com>
2026-02-26 00:34:11 -0300

    Create stamp conjunt

---
+++ felpojam-2026-game-code/scripts/entitys/stamps_conjunt.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+var packed_stamp := preload("res://scenes/entitys/stamp.tscn") as PackedScene
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+var stamps: Array[RunesData.names] = [
+	RunesData.names.fire,
+	RunesData.names.water,
+	RunesData.names.air,
+	RunesData.names.earth,
+	RunesData.names.dark,
+	RunesData.names.light
+]
+
+func _ready() -> void:
+	var stand_size := sprite_2d.texture.get_size()
+	var space := (stand_size.x/stamps.size())
+	var start_x = stand_size.x/-2 + 39
+	
+	for i in range(stamps.size()):
+		var stamp_scene := packed_stamp.instantiate() as Stamp
+		stamp_scene.rune_name = stamps[i]
+		stamp_scene.position.x = (start_x + space * i) - i
+		add_child(stamp_scene)
