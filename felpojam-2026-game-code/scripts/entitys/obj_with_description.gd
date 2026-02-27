a8bc014d2c74f46f19403fc63b902420b525bb3e
luankaike-code <luankaikemacksantos@gmail.com>
2026-02-27 02:00:24 -0300

    Fix obj with description

--- felpojam-2026-game-code/scripts/entitys/obj_with_description.gd
+++ felpojam-2026-game-code/scripts/entitys/obj_with_description.gd
@@ -5,8 +5,11 @@ var packed_speech_bubble := load("res://scenes/HUD/speech_bubble.tscn") as Packe
 var description: String
 var current_speech_bubble: SpeechBubble
 var mouse_is_hover := false
+var camera: Camera2D
 
 func _ready() -> void:
+	camera = get_viewport().get_camera_2d()
+	
 	mouse_entered.connect(_open_description_bubble)
 	mouse_exited.connect(_close_description_bubble)
 	mouse_entered.connect(func(): mouse_is_hover = true)
@@ -24,12 +27,32 @@ func _open_description_bubble():
 		return
 	
 	current_speech_bubble = packed_speech_bubble.instantiate()
+	
 	current_speech_bubble.setup([description], 0)
-	current_speech_bubble.position = get_local_mouse_position()
+	current_speech_bubble.position = _get_bubble_position(current_speech_bubble.min_width)
 	current_speech_bubble.typing_velocity = 0
 	current_speech_bubble.z_index = 4000
+	
 	add_child(current_speech_bubble)
 
 func _close_description_bubble():
 	if current_speech_bubble:
 		current_speech_bubble.destroy()
+
+func _get_viewport_left_border_x() -> float:
+	var viewport_rect_size := camera.get_viewport_rect().size/camera.zoom.x
+	var viewport_size_x := viewport_rect_size.x
+	var viewport_pos_x := camera.position.x - viewport_size_x/2
+	var viewport_x := viewport_size_x + viewport_pos_x
+	return viewport_x
+
+func _get_bubble_position(min_width: float) -> Vector2:
+	var pos := get_global_mouse_position()
+	var gap := 75
+	var bubble_x := pos.x + min_width + gap
+	var viewport_x := _get_viewport_left_border_x()
+	
+	var diference_x := viewport_x - bubble_x
+	
+	pos.x += diference_x-gap if diference_x < 0 else 0.0
+	return to_local(pos)
