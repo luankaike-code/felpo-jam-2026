1a2e9056df6218223f26b06f7f3dacc4a654f0be
luankaike-code <luankaikemacksantos@gmail.com>
2026-02-28 17:40:46 -0300

    Create basic stand paper

---
+++ felpojam-2026-game-code/scripts/entitys/paper_stand.gd
@@ -0,0 +1,38 @@
+extends Stand
+
+var data: Dictionary[int, Vector2]
+
+func place_item(item: Node2D) -> bool:
+	var cond := is_item_available(item)
+	
+	if cond:
+		data.get_or_add(item.get_instance_id(), item.position)
+		item.default_stand = self
+		
+		var into_collision := is_paper_into_collision(item)
+
+		if !into_collision:
+			item.position = data.get(item.get_instance_id())
+		else:
+			data[item.get_instance_id()] = item.position
+		
+	return cond
+
+func is_paper_into_collision(paper: Paper) -> bool:
+	var ids = get_shape_owners()
+	var into_collision := false
+	
+	for id in ids:
+		var collision: CollisionShape2D = shape_owner_get_owner(id)
+		var rect := collision.shape.get_rect()
+		print(id, " ", rect)
+		into_collision = rect.has_point(paper.position)
+		print(into_collision)
+		if into_collision:
+			break
+	
+	return into_collision
+	
+
+func is_item_available(item: Node2D) -> bool:
+	return item is Paper
