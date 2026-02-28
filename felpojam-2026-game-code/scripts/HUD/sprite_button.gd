562b67227dbc72ade51f6363ad08bd89616d9bcf
luankaike-code <luankaikemacksantos@gmail.com>
2026-02-28 16:55:19 -0300

    Add button sprite feadback

--- felpojam-2026-game-code/scripts/HUD/sprite_button.gd
+++ felpojam-2026-game-code/scripts/HUD/sprite_button.gd
@@ -10,6 +10,8 @@ class_name SpriteButton extends MarginContainer
 @onready var label: Label = $MarginContainer/Label
 @onready var button := $Button as TextureButton
 
+var tween: Tween
+
 signal button_up
 
 func _ready() -> void:
@@ -17,6 +19,15 @@ func _ready() -> void:
 	
 	button.button_up.connect(button_up.emit)
 	button.button_down.connect(_play_sound)
+	button.mouse_entered.connect(func(): set_scale_anim(1.2))
+	button.mouse_exited.connect(func(): set_scale_anim(1))
+
+func set_scale_anim(new_scale: float) -> void:
+	if tween:
+		tween.kill()
+	
+	tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2(new_scale, new_scale), 0.2)
 
 func _play_sound() -> void:
 	Sound.play_sound(SoundData.names.pressed_button)
